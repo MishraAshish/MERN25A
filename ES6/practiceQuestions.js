0c6e15929d682487b010c01a11b8a6c40de73d8c
Ashish Mishra <ash.ishu025@gmail.com>
2025-07-11 20:02:30 +0530

    practice questions

---
+++ ES6/practiceQuestions.js
@@ -0,0 +1,23 @@
+//Question :
+///////////////////////////
+
+let persons = [
+    {id : 1, name : "John", tags : "javascript"},
+    {id : 2, name : "Alice", tags : "dontnet"},
+    {id : 3, name : "Roger", tags : "java"},
+    {id : 4, name : "Adam", tags : "javascript"},
+    {id : 5, name : "Alex", tags : "java"}
+];
+
+//1. List the person with javascript tag
+//2. List the name of person using java and put programmer after their name, change the name key to Developer
+//3. If we have anyone with tag python
+//4. Find the number of unique tags and their count present in list
+//5. create a funtion with name multiply which accepts three parameters, and returns multiplication of all
+//   but if we dont pass any parameter it returns 0
+//6. create an array of 1 - 5 and add arr[newval] = at 6th place, print the output using for of and for in loop
+//7. create an example of const where we can update on property of the object, where it says const is mutable
+//8. create a for loop using var and let, print each value in timeout after 2 second and try to 
+//   demonstrate functional scope of var and lexical of let 
+//9. Create an example of object destructuring using student with details 
+//10. Demonstrate all aspects of array destructuring - including -> rest,  swap and other points
