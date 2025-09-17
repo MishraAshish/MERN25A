de8a31314bc760ae41a105b00b976463fb18fe00
Ashish Mishra <ash.ishu025@gmail.com>
2025-09-17 20:52:35 +0530

    cart implementation

--- ReactApp/src/App/CommonComponents/ProjectEnhancement.js
+++ ReactApp/src/App/CommonComponents/ProjectEnhancement.js
@@ -1,4 +1,13 @@
-// 19-Sept-2025
+// 17-Sept-2025 - Cart Implementation
+// Cart Implementation
+// Create New Cart Component using react hooks, functional component
+// Each Item in this component should be added from Product Component 
+// Now each Product should have general fields, like Name, Description, Rating, Price, Category (New Product Document/ Collection)
+// In Product component each item when we click to display details should also have a button "Add To Item" on click should add to New Cart
+// On Cart Component, Button for save to Checkout should save the cart item to database (New Cart Document/ Collection)
+
+
+// 15-Sept-2025
 // Product Component - Create Product and Display Them
 // Create a product component using functional component and hooks (try using useRef as react hook and implement accordingly)
 // Create productReducer use it in store.js and also write switch case to add products
